a622d97d008f870eaad9628221fefa0d69b1ecce
ryokyobss-arch <ryokyobss@gmail.com>
2026-01-08 21:44:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -14,15 +14,14 @@ const CONFIG = {
   // Love letter content
   letter: {
     paragraph1: [
-      'Happy first monthsarry, mahal ko! Grabe ang bilis ng panahon, parang kahapon lang nung hindi mo me pinapansin baby sa hindi ko malamang dahilan, but its just you being sealost palaa, but screw that. I am your girlfriend, and you are mine now, babyy ko!'
+      Happy first monthsarry, mahal ko! Grabe ang bilis ng panahon, parang kahapon lang nung hindi mo me pinapansin baby sa hindi ko malamang dahilan, but its just you being sealost palaa, but screw that. I am your girlfriend, and you are mine now, babyy ko!
     ],
     paragraph2: [
-      'I just want you to know that you are soo special to me, thank you for being you, and making my days more brighter by just being with me. I loveee you more than you know, mahal ko. I just want you to know how I am grateful to have someone as special as you, and you are the only one for me, so please stop doubting yourself and overthinking things, but even if you do, I will always be right here assuring you — we will always be together facing those doubts, and storms. I will never leave you — as much as you want me to stay.'
+      I just want you to know that you are soo special to me, thank you for being you, and making my days more brighter by just being with me. I loveee you more than you know, mahal ko. I just want you to know how I am grateful to have someone as special as you, and you are the only one for me, so please stop doubting yourself and overthinking things, but even if you do, I will always be right here assuring you — we will always be together facing those doubts, and storms. I will never leave you — as much as you want me to stay.
     ],
     paragraph3: [
-      'I am sorry we cannot celebrate our first monthsarry together mahal ko, as much as I want to, you already know the reason. Just know that I am grateful that I have such an understanding partner like you. More monthsarry, dates, headaches, and problems to come, babyy! I will always be here, I loveee youuu so muchieee!!
-'
-    ]
+      I am sorry we cannot celebrate our first monthsarry together mahal ko, as much as I want to, you already know the reason. Just know that I am grateful that I have such an understanding partner like you. More monthsarry, dates, headaches, and problems to come, babyy! I will always be here, I loveee youuu so muchieee!!
+  ]
   },
   
   // Time display text
